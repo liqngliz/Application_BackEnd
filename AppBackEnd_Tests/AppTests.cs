09c5a8c7c3f7740a0a0d5a69a1c8ee762f458ddc
liqngliz <liqngliz@gmail.com>
2023-11-22 22:07:40 +0100

    project bugfix

--- AppBackEnd_Tests/AppTests.cs
+++ AppBackEnd_Tests/AppTests.cs
@@ -5,7 +5,7 @@ namespace IDSP_Boiler_Plate_Tests;
 public class UnitTest1{
 
     [Fact]
-    public async void Test1()
+    public void Test1()
     {   
         var mockHttpHandler = new MockHttpHandler();
         var resp = new HttpResponseMessage(HttpStatusCode.Accepted);
